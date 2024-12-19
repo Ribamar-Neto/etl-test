f37a72e50ad698dfd0e1c2b58a92de6505ff5b6b
Ribamar Neto <ribamar.neto@delfosim.com>
2024-12-19 16:02:22 -0300

    Feat: Adding Date Filter for main route

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-from typing import Annotated 
+from typing import Annotated, Optional 
 from sqlalchemy.orm import Session
 from src.db_config.fonte.models import Data
-from fastapi import FastAPI, Depends, HTTPException, Path
+from fastapi import FastAPI, Depends, HTTPException, Path, Query
 from pydantic import BaseModel
 from src.db_config.fonte.database import Base, engine, SessionLocal
 from starlette import status
@@ -31,13 +31,27 @@ class DataRequest(BaseModel):
 
 
 @app.get('/data', status_code=status.HTTP_200_OK)
-async def read_all(db: db_dependency):
-    return db.query(Data).all()
+async def read_all(
+    db: db_dependency,
+    start_date: Optional[str] = Query("2024-12-19T17:00:00+00:00", alias="start"),
+    end_date: Optional[str] = Query("2024-12-29T16:59:00+00:00", alias="end"),
+    ):
+    query = db.query(Data)
+
+    if start_date:
+        start_date_obj = datetime.fromisoformat(start_date)  # Usando fromisoformat para tratar datas no formato ISO 8601
+        query = query.filter(Data.timestamp >= start_date_obj)
+    
+    if end_date:
+        end_date_obj = datetime.fromisoformat(end_date)  # Usando fromisoformat para tratar datas no formato ISO 8601
+        query = query.filter(Data.timestamp <= end_date_obj)
+    
+    return query.all()
 
 
 @app.get('/data/{data_id}', status_code=status.HTTP_200_OK)
-async def read_data(db: db_dependency, data_id: int = Path(gt=0)): # bd fica antes dos outros parâmetros.
-    data_model = db.query(Data).filter(Data.id == data_id).first() # first(): pega o 1º. Paa o "for".
+async def read_data(db: db_dependency, data_id: int = Path(gt=0)):
+    data_model = db.query(Data).filter(Data.id == data_id).first()
     if data_model is not None:
         return data_model
     raise HTTPException(status_code=404, detail='Data not found')
